cb780c6f7e96862aa6e3fd00295a2d285806a2b9
d.poloka <d.poloka@levi9.com>
2017-03-31 15:11:18 +0300

    maven structure

---
+++ src/main/java/hyp_hdn_sniffer_tests/App.java
@@ -0,0 +1,13 @@
+package hyp_hdn_sniffer_tests;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
